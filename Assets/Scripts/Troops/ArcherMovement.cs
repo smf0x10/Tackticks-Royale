fbafce585d3e8de47025c4061175c452b827db35
Sean Frett <smf0x10dev@gmail.com>
2021-10-02 15:55:09 -0500

    Made troop pathfinding only target troops close by, got the rally system mostly working (still needs lots of QoL improvements though), removed some commented-out code, and switched stat editing from the inspector to the script files

--- Assets/Scripts/Troops/ArcherMovement.cs
+++ Assets/Scripts/Troops/ArcherMovement.cs
@@ -1,10 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.Networking;
-
+﻿
 public class ArcherMovement : Troop
 {
-    
+    public override int GetMaxHp()
+    {
+        return 3;
+    }
+
+    public override int GetAtk()
+    {
+        return 1;
+    }
+
+    public override float GetAtkDelay()
+    {
+        return 2;
+    }
+
+    public override float GetKnockback()
+    {
+        return 30;
+    }
+
+    public override float GetAtkRange()
+    {
+        return 6;
+    }
+
+    public override float GetGetUpDelay()
+    {
+        return 2;
+    }
+
+    public override float GetMoveSpeed()
+    {
+        return 2;
+    }
 }
