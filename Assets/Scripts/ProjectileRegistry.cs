1857c23612448c8182c2920aa4b3e8414db619a1
Sean Frett <smf0x10dev@gmail.com>
2021-10-30 19:38:49 -0500

    Added a new subclass for ranged troops and used it to overhaul archers. They now do not lose any horizontal range when their target is below them, and their arrows now always appear to hit the target. Also removed some unnessesary variables

---
+++ Assets/Scripts/ProjectileRegistry.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Provides an easy access point for projectile prefabs
+/// </summary>
+public class ProjectileRegistry : MonoBehaviour
+{
+    [SerializeField] private GameObject[] projectileList;
+
+    public static ProjectileRegistry instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    /// <summary>
+    /// Returns the prefab for a projectile
+    /// </summary>
+    /// <param name="type">The type of projectile to get</param>
+    public static GameObject GetProjectilePrefab(ProjectileType type)
+    {
+        return instance.projectileList[(int)type];
+    }
+}
+
+public enum ProjectileType
+{
+    ARROW
+}
---
+++ Assets/Scripts/Troops/ProjectileTroop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileTroop : Troop
+{
+
+    protected override void Attack(Troop atkTarget)
+    {
+        base.Attack(atkTarget);
+        Vector3 shotVec = atkTarget.transform.position - transform.position;
+        Vector3 shotPos = (transform.position + atkTarget.transform.position) / 2;
+        GameObject shot = Instantiate(GetProjectile(), shotPos, Quaternion.FromToRotation(Vector3.forward, shotVec));
+        float shotDist = Vector3.Magnitude(shotVec);
+        shot.transform.localScale = new Vector3(1, 1, shotDist / 2);
+    }
+
+    /// <summary>
+    /// Returns the prefab for this troop to use as the projectile.
+    /// The projectile is purely visual; stats are all determined by the other stat functions
+    /// </summary>
+    /// <returns></returns>
+    protected GameObject GetProjectile()
+    {
+        Debug.Log(ProjectileRegistry.GetProjectilePrefab(ProjectileType.ARROW));
+        return ProjectileRegistry.GetProjectilePrefab(ProjectileType.ARROW);
+    }
+
+    protected override bool IsInRange(Transform other)
+    {
+        if (other.position.y > transform.position.y)
+        {
+            return base.IsInRange(other);
+        }
+        float heightAdvantage = transform.position.y - other.transform.position.y;
+        return Vector3.Distance(transform.position, other.position) <= Mathf.Sqrt(Mathf.Pow(GetAtkRange(), 2) + Mathf.Pow(heightAdvantage, 2));
+    }
+
+}
